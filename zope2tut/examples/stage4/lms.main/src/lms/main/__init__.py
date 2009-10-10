bf1c01e293363bd704e40697ecdcd01531c96869
Baiju Muthukadan <baiju.m.mail@gmail.com>
2009-10-10 12:49:52 +0000

    Bit spacing

--- zope2tut/examples/stage4/lms.main/src/lms/main/__init__.py
+++ zope2tut/examples/stage4/lms.main/src/lms/main/__init__.py
@@ -1,12 +1,16 @@
 from OFS.Folder import Folder
 
+
 def addLmsMain(dispatcher, id="lms"):
     """Add LMS"""
     dispatcher._setObject(id, LmsMain(id))
     return "LMS Installed: %s" % id
 
+
 class LmsMain(Folder):
+
     meta_type = "LMS"
+
 
 def initialize(registrar):
     registrar.registerClass(LmsMain,
--- zope2tut/examples/stage6/lms.main/src/lms/main/app.py
+++ zope2tut/examples/stage6/lms.main/src/lms/main/app.py
@@ -3,10 +3,13 @@ from Products.PageTemplates.PageTemplateFile import PageTemplateFile
 
 addLmsMain_form =  PageTemplateFile('addLmsMain_form', globals())
 
+
 def addLmsMain(dispatcher, id):
     """Add LMS"""
     dispatcher._setObject(id, LmsMain(id))
     return "LMS Installed: %s" % id
 
+
 class LmsMain(Folder):
+
     meta_type = "LMS"
